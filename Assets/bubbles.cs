3c89bfb87c55baf92e1b951db98be6323b8a5d6a
cozilas <itotcozilas@yahoo.com>
2019-12-24 23:33:13 +0200

    bubbling

---
+++ Assets/bubbles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubbles : MonoBehaviour
+{
+    public   GameObject player;
+    public float moveSpeed;
+    private GameObject col;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        col = gameObject.transform.Find("bubbleCollider").gameObject;
+
+        player.transform.position = Vector2.Lerp(player.transform.position, gameObject.transform.position,moveSpeed);
+        if(player.transform.position == gameObject.transform.position)
+        {
+            col.SetActive(true);
+
+            Debug.Log("suck yo mum");
+        }
+        
+
+      
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        col = gameObject.transform.Find("bubbleCollider").gameObject;
+        col.SetActive(false);
+    }
+}
+ 
