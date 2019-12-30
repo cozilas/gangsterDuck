20b6b388bfeb590bff9736f3cd7d37562dae9cf1
cozilas <itotcozilas@yahoo.com>
2019-12-30 12:47:03 +0200

    new duck new bubble002

--- Assets/bubbles.cs
+++ Assets/bubbles.cs
@@ -4,27 +4,70 @@ using UnityEngine;
 
 public class bubbles : MonoBehaviour
 {
-    public   GameObject player;
+    //player testing
     public float moveSpeed;
+
+    //rotate bubble animation
+    int rotate = 0;
+
     private GameObject col;
-    private void OnTriggerEnter2D(Collider2D other)
+    private bool isInBubble = false;
+    private double colRadiusSize = 0;
+    private bool notGrounded;
+    private bool isGrounded;
+
+    private void Start()
     {
-        col = gameObject.transform.Find("bubbleCollider").gameObject;
+        col = gameObject.transform.Find("bubbleCollider").gameObject;// remove it from lower;
+    }
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        player.transform.position = Vector2.Lerp(player.transform.position, gameObject.transform.position, moveSpeed);
 
-        player.transform.position = Vector2.Lerp(player.transform.position, gameObject.transform.position,Time.deltaTime*moveSpeed);
-        
-            col.SetActive(true);
+        col.SetActive(true);
 
-            Debug.Log("suck yo mum");
-        
-        
+        isInBubble = true;
+    }
+    private void OnTriggerExit2D(Collider2D player)//check if working 
+    {
+        col.SetActive(false);
+        isInBubble = false;
+        colRadiusSize = 0;
+        col.GetComponent<EdgeCollider2D>().edgeRadius = 0;
+        col.GetComponent<EdgeCollider2D>().enabled = true;
 
+    }
+    private void Update()
+    {
+        notGrounded = GameObject.Find("ducky").GetComponent<playerControls>().hasStoppedJumping;
+
+        if (colRadiusSize <= 0.66 && isInBubble)//increases the radious of the collider
+        {
+            colRadiusSize += 0.05f;
+
+            col.GetComponent<EdgeCollider2D>().edgeRadius += 0.05f;
+        }
+
+        if (isInBubble && notGrounded)//dissables bubbleCollider component
+        {
+            col.GetComponent<EdgeCollider2D>().enabled = false;/// DAME KAMI DISABLE TO COLLIDER             
+        }
+
+        //animate the bubbles
+        if(rotate < 5)
+        {
+            gameObject.transform.Rotate(0, 0, 1);
+            rotate++;
+        }
+        else
+        {
+            gameObject.transform.Rotate(0, 0, -1);
+            rotate++;
+            if(rotate == 10)
+            {
+                rotate = 0;
+            }
+        }
       
     }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        col = gameObject.transform.Find("bubbleCollider").gameObject;
-        col.SetActive(false);
-    }
 }
- 
